79009a1abbe036f84948db0052b5330018867b02
RamiCoding404 <ramiibrahim404@gmail.com>
2024-02-19 06:35:42 +0200

    fix-seed-error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,12 +4,13 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ** generate fake data for Todo model
-  await prisma.todo.createMany({
-    data: Array.from({ length: 25 }, () => ({
-      title: faker.lorem.words({ min: 2, max: 5 }),
-      body: faker.lorem.words({ min: 1, max: 10 }),
-    })),
-  });
+  // await prisma.todo.createMany({
+  //   data: Array.from({ length: 25 }, () => ({
+  //     title: faker.lorem.words({ min: 2, max: 5 }),
+  //     body: faker.lorem.words({ min: 1, max: 10 }),
+  //     user_id: faker.string.uuid(),
+  //   })),
+  // });
   // ** generate fake data for user model
   // await prisma.user.createMany({
   //   data: Array.from({ length: 25 }, () => ({
